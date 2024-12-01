b1abf516d26047583208d424ba3ed192bd8ce4df
Mathew Taylor <mat@mathewtaylor.co.uk>
2024-12-01 10:47:34 +0000

    day 1, part 2

--- day1/main.py
+++ day1/main.py
@@ -1,4 +1,5 @@
 import csv
+import itertools
 
 
 
@@ -16,15 +17,34 @@ def get_lists() -> tuple[list[int], list[int]]:
     return l1, l2
 
 
-def main():
-    list_one, list_two = get_lists()
+def part_one(list_one: list[int], list_two: list[int]) -> int:
     combined = zip(sorted(list_one), sorted(list_two))
 
     total_distance = 0
     for x, y in combined:
         total_distance += abs(y - x)
 
-    print(total_distance)
+    return total_distance
+
+
+def part_two(list_one, list_two) -> int:
+    groups = itertools.groupby(sorted(list_two))
+    counts = {}
+    for x, y in groups:
+        counts[x] = len(list(y)) 
+
+    similarity = 0
+    for x in list_one:
+        similarity += x * counts.get(x, 0)
+
+    return similarity
+
+
+def main():
+    list_one, list_two = get_lists()
+
+    print(part_one(list_one, list_two))
+    print(part_two(list_one, list_two))
 
 
 
