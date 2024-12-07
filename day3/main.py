8582205980be44613cab47e665ab15e55ea8e69e
Mathew Taylor <mat@mathewtaylor.co.uk>
2024-12-07 11:44:40 +0000

    Day 3, part 2

--- day3/main.py
+++ day3/main.py
@@ -1,6 +1,8 @@
-from itertools import chain
 import operator
 import re
+
+MUL_MATCH = "(mul\([0-9]+,[0-9]+\))"
+OPERATOR_MATCH = "((?:mul|don\'t|do)\((?:[0-9]+,[0-9]+)?\))"
 
 class Command:
     command: str
@@ -14,21 +16,41 @@ class Command:
         return self.command == other.command and self.arguments == other.arguments
 
     def calculate(self):
-        return operator.mul(*self.arguments)
+        if self.command == "mul":
+            return operator.mul(*self.arguments)
 
-def parse_commands_from_input(input: str) -> list[str]:
-    pattern = "(mul\([0-9]+,[0-9]+\))"
+        return 0
+
+def calculate_commands_tracker(commands):
+    track = True
+    sum = 0
+    for c in commands:
+        if c.command == "do":
+            track = True
+        elif c.command == "don\'t":
+            track = False
+        else:
+            if track:
+                sum += c.calculate()
+
+    return sum
+
+
+def parse_commands_from_input(input: str, pattern: str) -> list[str]:
     return re.findall(pattern, input)
 
 def create_commands_from_strings(command_strings: list[str]) -> list[Command]:
     commands = []
     for r in command_strings:
         command, rem = r.split("(")
-        rem, _ = rem.split(")")
-        args = rem.split(",")
-        args = [int(a) for a in args]
+        if command == "mul":
+            rem, _ = rem.split(")")
+            args = rem.split(",")
+            args = [int(a) for a in args]
 
-        commands.append(Command(command, args))
+            commands.append(Command(command, args))
+        else:
+            commands.append(Command(command, []))
 
     return commands
 
@@ -41,16 +63,28 @@ def load_input() -> str:
 
     return ("").join(input)
 
-def main():
-    input = load_input()
-    command_strings = parse_commands_from_input(input)
+def part_one(input) -> int:
+    command_strings = parse_commands_from_input(input, MUL_MATCH)
+    print(command_strings)
     commands = create_commands_from_strings(command_strings)
 
     sum = 0
     for c in commands:
         sum += c.calculate()
 
-    print(sum)
+    return sum
+
+def part_two(input) -> int:
+    command_strings = parse_commands_from_input(input, OPERATOR_MATCH)
+    commands = create_commands_from_strings(command_strings)
+
+    return calculate_commands_tracker(commands)
+
+
+def main():
+    input = load_input()
+    print(part_one(input))
+    print(part_two(input))
 
 
 if __name__ == "__main__":
--- day3/test.py
+++ day3/test.py
@@ -1,13 +1,13 @@
 
 import unittest
-from main import Command, create_commands_from_strings, parse_commands_from_input
+from main import MUL_MATCH, Command, create_commands_from_strings, parse_commands_from_input
 
 class CommandStringParserTest(unittest.TestCase):
 
     TEST_INPUT = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"
 
     def test_parser(self):
-        commands = parse_commands_from_input(self.TEST_INPUT)
+        commands = parse_commands_from_input(self.TEST_INPUT, MUL_MATCH)
 
         self.assertEqual(commands, ["mul(2,4)", "mul(5,5)", "mul(11,8)", "mul(8,5)"])
 
@@ -21,6 +21,9 @@ class CommandStringCreatorTest(unittest.TestCase):
         self.assertEqual(commands, [
             Command("mul", [2, 4]), Command("mul", [5, 5]), 
             Command("mul", [11, 8]), Command("mul", [8, 5])])
+
+    def test_create_commands_operators(self):
+        pass
 
 
 if __name__ == "__main__":
