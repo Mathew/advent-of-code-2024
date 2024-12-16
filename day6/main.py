636bd48821274c3c7ac8f25979758357c8caf6b2
Mathew Taylor <mat@mathewtaylor.co.uk>
2024-12-16 19:09:21 +0000

    Day 6, part 2 - oh mighty snake god I am sorry.

--- day6/main.py
+++ day6/main.py
@@ -1,4 +1,9 @@
+from copy import deepcopy
 from dataclasses import dataclass
+
+
+class InLoop(Exception):
+    pass
 
 
 @dataclass
@@ -8,6 +13,9 @@ class Position:
 
     def __add__(self, other):
         return Position(self.x + other.x, self.y + other.y)
+
+    def __sub__(self, other):
+        return abs((self.x - other.x) + (self.y - other.y))
 
     def __equals__(self, other):
         return self.x == other.x and self.y == other.y
@@ -20,6 +28,7 @@ class Guard:
     position: Position
     vector: Position
     walked: list[Position]
+    turn_positions: list[tuple[Position, Position]]
 
     UP = Position(0, -1)
     RIGHT = Position(1, 0)
@@ -30,21 +39,30 @@ class Guard:
         self.position = position
         self.walked = [position, ]
         self.vector = self.UP
+        self.turn_positions = []
 
-    def walk(self, grid):
+    def walk(self, grid, detect_loop=False):
         walking = True
         while(walking):
-            walking = self._step_once(grid)
+            walking = self._step_once(grid, detect_loop)
 
         return self._positions()
 
-    def _step_once(self, grid) -> bool:
+    def _step_once(self, grid, detect_loop=False) -> bool:
         lookahead = self.position + self.vector
 
-        if lookahead.x == len(grid[0]) or lookahead.y == len(grid):
+        if lookahead.x == len(grid[0]) or lookahead.y == len(grid) or lookahead.x < 0 or lookahead.y < 0:
             return False
 
         if grid[lookahead.y][lookahead.x] == "#":
+            if detect_loop:
+                tup = (self.position, self.vector)
+
+                if tup in self.turn_positions:
+                    raise InLoop()
+
+                self.turn_positions.append(tup)
+
             self._turn()
             return True
 
@@ -84,15 +102,39 @@ def find_guard(grid: list[list[str]]) -> Guard:
     raise Exception("Guard Not Found")
 
 
+def calculate_inf_loops(grid, guard):
+    inf_loops = 0
+    for pop in set(guard.walked[1:]):
+        pop_grid = deepcopy(grid)
+        guard = find_guard(pop_grid)
+        pop_grid[pop.y][pop.x] = "#"
+
+        if pop.y == guard.position.y and pop.x == guard.position.x:
+            continue
+
+        try:
+            guard.walk(pop_grid, True)
+        except InLoop:
+            inf_loops += 1
+
+    return inf_loops
+
+
 def part_one():
     grid = load_data()
     guard = find_guard(grid)
     unq_pos = guard.walk(grid)
-    print(len(unq_pos))
+
+
+def part_two():
+    grid = load_data()
+    guard = find_guard(grid)
+    guard.walk(grid)
 
 
 def main():
     part_one()
+    part_two()
 
 if __name__ == "__main__":
     main()
--- day6/test.py
+++ day6/test.py
@@ -1,7 +1,7 @@
 import unittest
 import pprint
 
-from main import Position, parse_grid, find_guard
+from main import Position, parse_grid, find_guard, calculate_inf_loops
 
 GRID = parse_grid([
     "....#.....",
@@ -20,6 +20,13 @@ class TestGrid(unittest.TestCase):
 
     def test_find_guard(self):
         self.assertEqual(find_guard(GRID).position, Position(4, 6))
+
+    def test_obstacles(self):
+        guard = find_guard(GRID)
+        guard.walk(GRID)
+
+        self.assertEqual(calculate_inf_loops(GRID, guard), 6)
+
 
 if __name__ == "__main__":
     unittest.main()
